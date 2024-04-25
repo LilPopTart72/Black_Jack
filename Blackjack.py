ab219dd3b883b916560112c2bc2126bc51a00a01
Lilpoptart72 <rileythompson0220@gmail.com>
2024-04-25 15:09:02 -0500

    Dealer Ace and Win / Lose v1

--- Blackjack.py
+++ Blackjack.py
@@ -1,26 +1,49 @@
 import Cards
 import InputCommands as IC
-import os
 
 # print(Cards.init.deck)
 Cards.init.shuffle()
-player = Cards.UserCards()
-dealer = Cards.UserCards()
+player = Cards.UserCards("Player")
+dealer = Cards.UserCards("Dealer")
 
 
-def basic_print():
+def basic_print(code):
     print(" ")
     print(player.cards)
     print("Player score:", player.score)
 
     print(dealer.cards)
     print("Dealer score:", dealer.score)
-    return IC.play(input("Would you like to hit or stay? ").lower(), player)
+    if code == 1:
+        return IC.play(input("Would you like to hit or stay? ").lower(), player)
+
+
+def win_lose(code):
+    if code == 1:
+        choice = basic_print(1)
+        if player.score > 21:
+            print("you Busted")
+            return "stop"
+    else:
+        basic_print(0)
+        if dealer.score > 21:
+            print("Dealer Busted")
+        elif dealer.score > player.score:
+            print("Dealer Won")
+        elif dealer.score < player.score:
+            print("you Won")
+        elif dealer.score == player.score:
+            print("its a tie")
+    return choice
 
 
 while True:
-    choice = basic_print()
+    choice = win_lose(1)
     if choice == "stay":
+        x = "continue"
         break
-
-
+    elif choice == "stop":
+        x = choice
+if x == "continue":
+    dealer.dealer_new_card()
+    win_lose(0)
--- Cards.py
+++ Cards.py
@@ -2,7 +2,7 @@ import random
 import InputCommands as IC
 
 
-class Card():
+class Card:
 
     def __init__(self):
         self.suit = ['Spades', 'Clubs', 'Diamonds', 'Hearts']
@@ -17,15 +17,23 @@ class Card():
         print(suit, value)
 
 
-class UserCards():
-    def __init__(self):
+class UserCards:
+    def __init__(self, n):
         self.cards = [init.deck.pop(0), init.deck.pop(0)]
         self.score = 0
+        self.name = n
         self.value_of_card()
 
     def new_card(self):
         self.cards.append(init.deck.pop(0))
         self.value_of_card()
+
+    def dealer_new_card(self):
+        if self.score < 17:
+            self.cards.append(init.deck.pop(0))
+            self.value_of_card()
+        else:
+            return
 
     def value_of_card(self):
         self.score = 0
@@ -34,9 +42,35 @@ class UserCards():
                 self.score += int(card[0])
             elif type(card[0]) is str:
                 if card[0] == "Ace":
-                    self.score += IC.ace(input("Is this Ace a 1 or 11? "))
+                    if self.name == "Dealer":
+                        self.dealer_ace(card)
+                    elif self.name == "Player":
+                        self.player_ace(card)
                 elif card[0] == "Jack" or "Queen" or "King":
                     self.score += 10
+
+    def dealer_ace(self, card):
+        x = self.cards.index(card)
+        if len(card) == 3:
+            self.score += int(card[2])
+        else:
+            if self.score + 11 <= 21:
+                self.cards[x].append(11)
+                self.score += 11
+            else:
+                self.cards[x].append(1)
+                self.score += 1
+
+    def player_ace(self, card):
+        x = self.cards.index(card)
+        if len(card) == 3:
+            self.score += int(card[2])
+        else:
+            print(" ")
+            print(self.cards)
+            value = IC.ace(input("Is this Ace a 1 or 11? "))
+            self.cards[x].append(value)
+            self.score += value
 
 
 init = Card()
